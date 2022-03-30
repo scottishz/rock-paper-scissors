4458aa929b8f27e63bdfaedfc43fc2f84eb6381c
scottishz <ztelfer@gmail.com>
2022-03-30 13:27:22 -0700

    Create game function to play 5 games and record score

--- rps.js
+++ rps.js
@@ -1,9 +1,7 @@
 console.log('Play a game of Rock Paper scissors vs the computer. Make selection below:')
-let playerInput = prompt();
-let computerInput = computerPlayed();
-playOneGame(playerInput, computerInput);
+game();
 
-
+//Function to return random computer choice between rock paper and scissors
 function computerPlayed() {
     let computerChoice;
     let computerHand = Math.random();
@@ -23,9 +21,13 @@ function computerPlayed() {
     return computerChoice;
 }
 
+//Takes in player selection and computerPlayed's return value, switch case for players selection and then if else to determine winner
+//returns one of three values (tie, playerWin, computerWin)
 function playOneGame(playerSelection, computerSelection) {
 
     let outcome;
+
+    
 
     switch(playerSelection.toLowerCase())
     {
@@ -41,8 +43,10 @@ function playOneGame(playerSelection, computerSelection) {
                 console.log('You Lose! Paper beats Rock');
             }
             else   
-            {    outcome = 'playerWin';
-                console.log('You Win! Rock beats Scissors');}
+            {   
+                outcome = 'playerWin';
+                console.log('You Win! Rock beats Scissors');
+            }
                 break;
 
         case 'paper':
@@ -57,26 +61,75 @@ function playOneGame(playerSelection, computerSelection) {
             console.log('Tie Match');
         }
         else   
-{            outcome = 'computerWin';
-            console.log('You Lose! Scissors beats Paper');}
+            {         
+            outcome = 'computerWin';
+            console.log('You Lose! Scissors beats Paper');
+            }
             break;
 
-        case 'scissors':
-            if(computerSelection == 'Rock')
-            {
-                outcome = 'computerWin';
-                console.log('You Lose! Rock beats Scissors');
-            }
-        else if(computerSelection == 'Paper')
-        {
-            outcome = 'playerWin';
-            console.log('You Win! Scissors beats Paper');
+            case 'scissors':
+                if(computerSelection == 'Rock')
+                {
+                    outcome = 'computerWin';
+                    console.log('You Lose! Rock beats Scissors');
+                }
+            else if(computerSelection == 'Paper')
+                {
+                outcome = 'playerWin';
+                console.log('You Win! Scissors beats Paper');
+                }
+            else   
+    {            outcome = 'tie';
+                console.log('Tie Match!');}
+                break;
         }
-        else   
-{            outcome = 'tie';
-            console.log('Tie Match!');}
-            break;
+       
+        return outcome;
     }
-   
-    return outcome;
+    
+//Plays a 5 round game
+function game() {
+
+    let computerWins = 0;
+    let playerWins = 0;
+    let result;
+    let playerCancel = false;
+    let computerInput = computerPlayed();
+    let playerInput = prompt();
+
+
+    do
+    {
+        if(playerInput === null)
+        {
+            playerCancel = true;
+        }
+        else{
+        result = playOneGame(playerInput, computerInput);
+
+        if(result === 'playerWin')
+           { playerWins++;
+            console.log('The current score is Player: ' + playerWins + ' Computer: ' + computerWins )}
+        else if(result === 'computerWin')
+           { 
+            computerWins++
+            console.log('The current score is Player: ' + playerWins + ' Computer: ' + computerWins )
+        }
+        else if(result === 'tie')
+            console.log('The current score is Player: ' + playerWins + ' Computer: ' + computerWins )
+            else
+            {
+            console.log('Something went wrong. Make sure you are spelling correctly')
+            }
+
+            computerInput = computerPlayed();
+            playerInput = prompt();
+        }
+    } while (playerWins < 3 && computerWins < 3 && !playerCancel)
+
+    if(playerCancel == true)
+        console.log('No input received. To start a new game, refresh the page')
+    else
+        console.log('The game is over. Final Score -> Player: ' + playerWins + ' Computer: ' + computerWins)
+
 }
